102ed05b26de406f9f902110ad256fb3c2d35133
s14007 <s14007@std.it-college.ac.jp>
2015-06-02 11:05:50 +0900

    Initial commit

---
+++ iterator/src/jp/ac/it_college/std/s14007/pdp/Aggregate.java
@@ -0,0 +1,10 @@
+package jp.ac.it_college.std.s14007.pdp;
+
+/**
+ * Created by s14007 on 15/06/02.
+ */
+public class Aggregate {
+    public interface Iterator{
+
+    }
+}
---
+++ src/jp/ac/it_college/std/s14007/pdp/Main.java
@@ -0,0 +1,8 @@
+package jp.ac.it_college.std.s14007.pdp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
