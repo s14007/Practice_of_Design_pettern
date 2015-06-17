4b0e476809e66da302681a560cc4ef000059d7ca
s14007 <s14007@std.it-college.ac.jp>
2015-06-17 16:06:22 +0900

    20章終わり

---
+++ visitor/src/jp/ac/it_college/std/s14007/pdp/visitor/Entry.java
@@ -0,0 +1,22 @@
+package jp.ac.it_college.std.s14007.pdp.visitor;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by s14007 on 15/06/17.
+ */
+public abstract class Entry implements Element {
+    public abstract String getName();
+    public abstract int getSize();
+    public Entry add(Entry entry) throws FileTreatmentException {
+        throw new FileTreatmentException();
+    }
+    public Iterator iterator() throws FileTreatmentException {
+        throw new FileTreatmentException();
+    }
+
+    public String toString() {
+        return getName() + "(" + getSize() +")";
+    }
+}
